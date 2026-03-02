5b9b4d91ccb71ba49efe7b71cfc302925b808dfb
jaime <57179514+jaimechaves222@users.noreply.github.com>
2026-03-02 00:54:41 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,23 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyC8uifBlca20xi1P4pxTzYlWOgcMjs-1fU",
-  authDomain: "coffee-spark-ai-barista-8d97b.firebaseapp.com",
-  projectId: "coffee-spark-ai-barista-8d97b",
-  storageBucket: "coffee-spark-ai-barista-8d97b.firebasestorage.app",
-  messagingSenderId: "354237817776",
-  appId: "1:354237817776:web:40b6fbc62fbfa2eb16ec75"
-};
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyAh4xwGI7hVn4yvjKYoDzgLAayMpXcxiRQ",
+    authDomain: "ficha-36ff7.firebaseapp.com",
+    projectId: "ficha-36ff7",
+    storageBucket: "ficha-36ff7.firebasestorage.app",
+    messagingSenderId: "786365787379",
+    appId: "1:786365787379:web:7b2813f7519aae903b1b86",
+    measurementId: "G-4FQ1Z9V5FT"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
